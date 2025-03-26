6ea6a81659cf19c249c3c75460c6e12f6b590164
wanghw0122 <wanghw0122@126.com>
2025-03-27 02:41:15 +0800

    t

--- ezMoney/run.py
+++ ezMoney/run.py
@@ -270,9 +270,33 @@ strategies = {
                     }
                 ]
             },
-            "断低吸": {
+            # "断低吸": {
+            #     # 10日 高频 前2 有方向 最近强势 创建日期 2025-03-19
+            #     "code": "9G0032",
+            #     "returnNum": 3,
+            #     "budget": "ddx",
+            #     'returnFullInfo': True,
+            #     'filter_params': [
+            #         {
+            #         'filtered': True,
+            #         'fx_filtered': True,
+            #         'topn': 1,
+            #         'top_fx': 2,
+            #         'top_cx': 1,
+            #         'only_fx': True,
+            #         'enbale_industry': False,
+            #         'empty_priority': False,
+            #         'min_trade_amount': 12000000,
+            #         'block_rank_filter': True,
+            #         'gap': 0,
+            #         'except_is_ppp': True,
+            #         'except_is_track': False
+            #         }
+            #     ]
+            # },
+            "高强中低开低吸": {
                 # 10日 高频 前2 有方向 最近强势 创建日期 2025-03-19
-                "code": "9G0032",
+                "code": "9G0128",
                 "returnNum": 3,
                 "budget": "ddx",
                 'returnFullInfo': True,
@@ -282,11 +306,11 @@ strategies = {
                     'fx_filtered': True,
                     'topn': 1,
                     'top_fx': 2,
-                    'top_cx': 1,
+                    'top_cx': 3,
                     'only_fx': True,
                     'enbale_industry': False,
                     'empty_priority': False,
-                    'min_trade_amount': 12000000,
+                    'min_trade_amount': 10000000,
                     'block_rank_filter': True,
                     'gap': 0,
                     'except_is_ppp': True,
@@ -294,7 +318,30 @@ strategies = {
                     }
                 ]
             },
-            
+            "低位高强低吸": {
+                # 10日 高频 前2 有方向 最近强势 创建日期 2025-03-19
+                "code": "9G0124",
+                "returnNum": 5,
+                "budget": "ddx",
+                'returnFullInfo': True,
+                'filter_params': [
+                    {
+                    'filtered': True,
+                    'fx_filtered': True,
+                    'topn': 1,
+                    'top_fx': 1,
+                    'top_cx': 1,
+                    'only_fx': False,
+                    'enbale_industry': False,
+                    'empty_priority': False,
+                    'min_trade_amount': 6000000,
+                    'block_rank_filter': True,
+                    'gap': 0,
+                    'except_is_ppp': True,
+                    'except_is_track': False
+                    }
+                ]
+            },
             "绿盘低吸": {
                 # 30日 中频 前2 有方向 最近强势 创建日期 2025-03-20
                 "code": "9G0002",
@@ -387,7 +434,8 @@ strategies_to_buffer = {
     "低吸-中位低吸": [0.015],
     "低吸-中位断板低吸": [0.02],
     "低吸-连断低吸": [0.02],
-    "低吸-断低吸": [0.015],
+    "低吸-高强中低开低吸": [0.018],
+    "低吸-低位高强低吸": [0.015],
     "低吸-放宽低吸前3": [0.015],
     "低吸-绿盘低吸": [0.015],
     "追涨-小高开追涨": [0.02],
@@ -400,7 +448,8 @@ default_positions = {
     "低吸-低位孕线低吸": 0.25,
     "低吸-连断低吸": 0.2,
     "低吸-中位断板低吸": 0.1,
-    "低吸-断低吸": 0.25,
+    "低吸-高强中低开低吸": 0.2,
+    "低吸-低位高强低吸": 0.2,
     "低吸-放宽低吸前3": 0.25,
     "追涨-小高开追涨": 0.2,
     "低吸-绿盘低吸": 0.25,

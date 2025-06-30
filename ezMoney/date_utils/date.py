3812a3884b68e1b728142a3cd0271e47a47188e0
wanghw0122 <wanghw0122@126.com>
2025-07-01 00:17:36 +0800

    a

--- ezMoney/date_utils/date.py
+++ ezMoney/date_utils/date.py
@@ -146,7 +146,7 @@ def is_trade_date(date_str):
 
 def is_between_925_and_930():
     now = datetime.now()
-    start_time = now.replace(hour=9, minute=26, second=15, microsecond=0)
+    start_time = now.replace(hour=9, minute=25, second=55, microsecond=0)
     end_time = now.replace(hour=9, minute=30, second=0, microsecond=0)
     return start_time <= now < end_time
 
